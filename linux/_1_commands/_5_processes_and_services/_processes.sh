fed7e4411f96beb9d215dd7cc38117fba06eb8b9
Vikram Kumar <vikram.kuamr@squareboat.com>
2022-08-29 16:24:50 +0530

    files name modified

---
+++ linux/_1_commands/_5_processes_and_services/_processes.sh
@@ -0,0 +1,7 @@
+# list of running processes:
+ps
+# PID: Process ID
+# TTY: Controlling terminal associated with the process (we'll go in detail about this later)
+# STAT: Process status code
+# TIME: Total CPU usage time
+# CMD: Name of executable/command
