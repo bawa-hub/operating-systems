4a80c88d93430d901c60a7f9e1e9e2d9e70a9768
bawa-hub <itsworkingmind@gmail.com>
2022-09-04 07:39:57 +0530

    basic commands added

---
+++ linux/_1_commands/_1_basic/_2_.sh
@@ -0,0 +1,18 @@
+# show all shells present in system
+cat /etc/shells 
+
+# executable permission to file
+chmod +x <filename>
+
+# logged in users details
+who 
+w # for detailed details
+
+# process details
+top
+htop # show detailed details
+
+# network status
+netstat
+netstat -tupln
+sudo netstat -tupln # for more info 
