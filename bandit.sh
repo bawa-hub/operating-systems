f34c80545114c0dd574a7941c20010d80f783f7c
bawa-hub <itsworkingmind@gmail.com>
2022-09-17 17:34:26 +0530

    command added

---
+++ bandit.sh
@@ -0,0 +1,2 @@
+# bandit6 pass 
+# P4L4vucdmLnm8I7Vl7jG1ApGSfjYKqJU
--- linux/_1_commands/_10_networking_tools/_1_.sh
+++ linux/_1_commands/_10_networking_tools/_1_.sh
@@ -19,3 +19,6 @@ sudo netstat -tupln # for more info
 
 # Generate SSH key --
 ssh-keygen -t rsa
+
+# ssh into remote host
+ssh user@host_address -p <port_no> # ssh default port is 20
